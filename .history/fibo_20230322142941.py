6a3f77344abcd005451c0261e24df1688f0c1480
Henrique Oliveira <henriquevro@gmail.com>
2023-03-22 14:36:13 -0300

    questao 3

---
+++ .history/fibo_20230322142941.py
@@ -0,0 +1,12 @@
+def fibo_check(number):
+    primeiro_valor = 0
+    segundo_valor = 1
+    while segundo_valor < number:
+        primeiro_valor, segundo_valor = segundo_valor, primeiro_valor + segundo_valor # Atribuição múltipla.
+    if segundo_valor == number:
+        return f'O número {number} pertence à sequência de Fibonacci.'
+    else:
+        return f'O número {number} não pertence à sequência de Fibonacci.'
+
+print(fibo_check(4181))  # O número 4181 pertence à sequência de Fibonacci.
+print(fibo_check(256))   # O número 256 não pertence à sequência de Fibonacci.
---
+++ .history/questao3/faturamento_20230322143511.py
@@ -0,0 +1,22 @@
+import json
+
+# Lê a lista de faturamento diário em formato JSON
+with open('dados.json', 'r') as f:
+    faturamento_diario = json.load(f)
+
+# Calcula o menor e o maior valor de faturamento diário
+menor_valor = min(faturamento_diario)
+maior_valor = max(faturamento_diario)
+
+# Calcula a média mensal de faturamento diário, ignorando os dias sem faturamento
+soma_faturamento = sum(faturamento_diario)
+num_dias_com_faturamento = len([f for f in faturamento_diario if f > 0])
+media_mensal = soma_faturamento / num_dias_com_faturamento
+
+# Conta o número de dias em que o valor de faturamento diário foi superior à média mensal
+dias_acima_media = len([f for f in faturamento_diario if f > media_mensal])
+
+# Imprime os resultados
+print("Menor valor de faturamento diário:", menor_valor)
+print("Maior valor de faturamento diário:", maior_valor)
+print("Número de dias com faturamento acima da média mensal:", dias_acima_media)
